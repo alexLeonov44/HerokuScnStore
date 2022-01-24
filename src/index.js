15a83eb238113a5ab5afaa85a85834f49011560f
Alexander <leonovfx.91@gmail.com>
2022-01-24 09:38:34 +0300

    test

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://heroku-scn-store-appol-server.herokuapp.com',
+  uri: '/graphql',
   cache: new InMemoryCache(),
-  credentials: true
 });
+
+// uri: 'http://heroku-scn-store-appol-server.herokuapp.com',
+// cache: new InMemoryCache(),
+// credentials: true
 
 ReactDOM.render(
   <React.StrictMode>
