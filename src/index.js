1bb0816da65993b1cebce4bfbe6fd16e87ecc911
Alexander <leonovfx.91@gmail.com>
2022-01-24 11:42:24 +0300

    add server adres

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
  const client = new ApolloClient({
 
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://heroku-scn-store-appol-server.herokuapp.com/graphql',
 cache: new InMemoryCache(),
 // credentials: true
 
