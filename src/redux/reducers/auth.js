a8f132c32827b953bd33e8af464b94dd4ddaaa56
Alexander <leonovfx.91@gmail.com>
2022-02-06 11:44:33 +0300

    reducer refactoring

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,30 +1,25 @@
-import { SET_AUTH_DATA } from "../types/auth";
-
+import { SET_AUTH_DATA } from '../types/auth';
 
 const initialState = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    saveMe: false,
-    isAuth:false
-  };
-  
-  const auth = (state = initialState, action) => {
-    switch (action.type) {
-      case SET_AUTH_DATA:
-        return {
-            ...state,
-            firstName: action.authData?.firstName
-            ,lastName:action.authData?.lastName
-            ,email:action.authData?.email
-            ,saveMe:action.authData?.saveMe
-            ,isAuth:action.authData ? true : false
-        };
-     
-      default:
-        return state;
-    }
-  };
-  
-  export default auth;
-  
+  firstName: '',
+  lastName: '',
+  email: '',
+  saveMe: false,
+  isAuth: false,
+};
+
+const auth = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_AUTH_DATA:
+      return {
+        ...state,
+        ...action.authData,
+        isAuth: action.authData ? true : false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default auth;
